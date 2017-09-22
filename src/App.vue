425a6b63fd127520c5ed5b46c58459186ff08cb4
infernalslam <infernal-slam@hotmail.com>
2017-09-22 11:45:00 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans');
+body {
+  margin: 0;
+  background-color: #000;
+}
+
+#app {
+  font-family: 'Noto Sans', sans-serif;s
+  color: #2c3e50;
+}
+
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="centered1">
+      10
+    </div>
+    <div class="centered2">
+      17
+    </div>
+    <div class="centered3"> {{ date }} </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      date: moment().format('LLLL')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.centered1 {
+  position: fixed;
+  top: 45%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+  color: #fff;
+  font-size: 50px;
+  padding-left: 0px;
+}
+
+.centered2 {
+  position: fixed;
+  top: 52%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+  color: #fff;
+  font-size: 50px;
+  padding-left: 0px;
+}
+.centered3 {
+  position: fixed;
+  top: 60%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
+  color: #fff;
+  font-size: 12px;
+  padding-left: 0px;
+}
+
+</style>
